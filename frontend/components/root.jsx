c1006bc29e03945e0f3a73f05f6289b59148d806
Sergio Medina <medinaommedina@gmail.com>
2020-11-16 15:34:31 -0800

    Project ready to go before Heroku

---
+++ frontend/components/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+const Root = () => {
+
+    return (
+        <div>
+
+        <p>Welcome to My Yelpy</p>
+        
+        </div>
+    )
+}
+
+export default Root;
