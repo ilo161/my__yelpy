c0ec15141446a5c23cc2f67aea83b9d5f175675f
Sergio Medina <medinaommedina@gmail.com>
2020-11-24 08:18:08 -0800

    Added In Biz Show - Header Box - title/stars(non dynamic)/ category/ edit button(non dynamic)

--- frontend/components/business/business_header_box.jsx
+++ frontend/components/business/business_header_box.jsx
@@ -1,20 +1,54 @@
 import React from 'react';
 import {withRouter} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faCheckCircle, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 const businessHeaderBox = props => {
 
     const element = <FontAwesomeIcon icon={faStar} />
+    const checked = <FontAwesomeIcon icon={faCheckCircle} />
+    const dollar = <FontAwesomeIcon icon={faDollarSign} />
 
 
     // helpemethod...to get stars
     return (
         <div className="business-header-container"> 
-            <div className="flex-row-start">
-                <p className="business-title">{props.business.business_name} <span> claimed</span></p>
+            <div className="flex-row-start bottom-m-8">
+                {/* FlexBox Start */}
+                <div className="business-title">{props.business.business_name} </div>
+                <p className="checked-claimed">{checked} Claimed</p>
             </div>
-            <div className="rating-icons"><span>{element}</span></div>
+            {/* Flexx End ^^ */}
+            <div className="flex-row-start flex-row-align bottom-m-8">
+                {/* FlexxBox Start */}
+                <div className="star-background">
+                    <div className="rating-icons"><span>{element}</span></div>
+                </div>
+                <div className="star-background">
+                    <div className="rating-icons"><span>{element}</span></div>
+                </div><div className="star-background">
+                    <div className="rating-icons"><span>{element}</span></div>
+                </div><div className="star-background">
+                    <div className="rating-icons"><span>{element}</span></div>
+                </div><div className="star-background">
+                    <div className="rating-icons"><span>{element}</span></div>
+                </div>
+
+                <p className="business-num-reviews">500 Reviews</p>
+                <p className="business-details-reviews">Details</p>
+            </div>
+            {/* Flex End ^^ */}
+            <div className="flex-row-start">
+                {/* Flexx Start */}
+                <div className="icon-space">{dollar}</div>
+                <div className="icon-space">{dollar}</div>
+                <div className="icon-space">{dollar}</div>
+                <div className="icon-space">{dollar}</div>
+                <div className="icon-space">{dollar}</div>
+
+                <p className="business-details-category-text"> Place holder Text of Category</p>
+                <button className="small-edit-category-business-button">Edit</button>
+            </div>
 
             
         </div>
