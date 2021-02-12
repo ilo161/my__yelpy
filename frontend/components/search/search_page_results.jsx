fb2346a2e692df241d78642d3c66c9dfd8514238
Sergio Medina <medinaommedina@gmail.com>
2021-02-11 20:35:17 -0800

    add full sections to sidebar

--- frontend/components/search/search_page_results.jsx
+++ frontend/components/search/search_page_results.jsx
@@ -39,26 +39,119 @@ class SearchPageResults extends React.Component{
                                 </div>
                                 {/* suggested checkboxes */}
                                 <div className="sidebar-seperator">
-                                    <div className="sidebar-header text-size-large">
+                                    <div className="sidebar-header text-size-md pd-b-8">
                                         <p>Suggested</p>
+                                    </div>
+                                    <div className="sidebar-selector-container flex-col-start">
+                                        {/* checkbox 1 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Curbside Pickup</span>
+                                        </label>
+                                        {/* checkbox 2 */}
+                                         <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Open Now</span>
+                                        </label>
+                                        {/* checkbox 3 */}
+                                         <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Yelp Delivery</span>
+                                        </label>
                                     </div>
                                 </div>
                                 {/* Category */}
                                 <div className="sidebar-seperator">
-                                    <div className="sidebar-header text-size-large">
+                                    <div className="sidebar-header text-size-md pd-b-8">
                                         <p>Category</p>
+                                    </div>
+
+                                    <div className="filter-toggle-set">
+                                        {/* 1 */}
+                                        <div className="pill-cat-container">
+                                            <button className="pill-button">
+                                                <span className="text-size-small semi-bold-plus" >Coffee & Tea</span>
+                                            </button>
+                                        </div>
+                                         {/* 2 */}
+                                        <div className="pill-cat-container">
+                                            <button className="pill-button">
+                                                <span className="text-size-small semi-bold-plus" >Food</span>
+                                            </button>
+                                        </div>
+                                         {/* 3 */}
+                                        <div className="pill-cat-container">
+                                            <button className="pill-button">
+                                                <span className="text-size-small semi-bold-plus" >Restaurants</span>
+                                            </button>
+                                        </div>
+                                         {/* 4 */}
+                                        <div className="pill-cat-container">
+                                            <button className="pill-button">
+                                                <span className="text-size-small semi-bold-plus" >Grocery</span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                                 {/* Features */}
                                 <div className="sidebar-seperator">
-                                    <div className="sidebar-header text-size-large">
+                                    <div className="sidebar-header text-size-md pd-b-8">
                                         <p>Features</p>
+                                    </div>
+                                    <div className="sidebar-selector-container flex-col-start">
+                                        {/* checkbox 1 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Yelp Takeout</span>
+                                        </label>
+                                        {/* checkbox 2 */}
+                                         <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Good for Kids</span>
+                                        </label>
+                                        {/* checkbox 3 */}
+                                         <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Accepts Apple Pay</span>
+                                        </label>
+                                        {/* checkbox 4 */}
+                                         <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk" type="checkbox"></input>
+                                             <span className="checkbox-text">Outdoor Seating</span>
+                                        </label>
                                     </div>
                                 </div>
                                 {/* Distance */}
                                 <div className="sidebar-seperator">
-                                    <div className="sidebar-header text-size-large">
+                                    <div className="sidebar-header text-size-md pd-b-8">
                                         <p>Distance</p>
+                                    </div>
+                                     <div className="sidebar-selector-container flex-col-start">
+                                        {/* checkbox 1 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk input-cir" type="checkbox"></input>
+                                             <span className="checkbox-text">Bird's Eye View</span>
+                                        </label>
+                                         {/* checkbox 2 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk input-cir" type="checkbox"></input>
+                                             <span className="checkbox-text">Driving (5 mi)</span>
+                                        </label>
+                                         {/* checkbox 3 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk input-cir" type="checkbox"></input>
+                                             <span className="checkbox-text">Biking (2 mi.)</span>
+                                        </label>
+                                         {/* checkbox 4 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk input-cir" type="checkbox"></input>
+                                             <span className="checkbox-text">Walking (1 mi.)</span>
+                                        </label>
+                                         {/* checkbox 5 */}
+                                        <label className="checkbox-row flex-row-start">
+                                             <input className="input-chk input-cir" type="checkbox"></input>
+                                             <span className="checkbox-text">Within 4 blocks</span>
+                                        </label>
                                     </div>
                                 </div>
                             </div>
