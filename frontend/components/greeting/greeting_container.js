4c52c2dd0428907f7d2c477d30e544be446e5812
Sergio Medina <medinaommedina@gmail.com>
2021-05-02 17:46:06 -0700

    cease query to aws for all biz

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Greeting from "./greeting";
 import {requestLogout, requestLogin} from "../../actions/session_actions";
-import {requestBusinesses} from "../../actions/business_actions";
+import {requestBusinesses, requestBusiness} from "../../actions/business_actions";
 
 const mSTP = (state) => {
 
@@ -17,7 +17,8 @@ const mDTP = (dispatch) => {
     return {
         requestLogout: () => dispatch(requestLogout()),
         requestLogin: (credentials) => dispatch(requestLogin(credentials)),
-        requestAllBusinesses: () => dispatch(requestBusinesses())
+        requestAllBusinesses: () => dispatch(requestBusinesses()),
+        requestBusiness: (bizId) => dispatch(requestBusiness(bizId))
     }
 }
 
